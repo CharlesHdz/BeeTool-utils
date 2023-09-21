35e97bf2c221488d6e2da13de42742abebf8f774
Juan M Barrios <j.m.barrios@gmail.com>
2023-09-21 15:28:31 -0600

    Se agrega el script de descarga de registros
    Se agrego el script para realizar la descarga de los archivos de abejas consideradas en el proyecto

--- Registros_abejas.R
+++ Registros_abejas.R
@@ -4,24 +4,25 @@ library(rgbif)
 
 #Lista de especies a descargar#
 
-lista <- read.csv("lista_abejas.csv", header=T)
+lista <- read_csv("data/bee-sps.csv")
+
+lista <- lista %>%
+  mutate(scientificName = paste(Género, Especie))
 
 # match the names
 gbif_taxon_keys <- lista %>%
-head(1000) %>% # only first 1000 names
-name_backbone_checklist() %>% # match to backbone
-filter(!matchType == "NONE") %>% # get matched names
-pull(usageKey)
+  pull(scientificName) %>%
+  head(1000) %>% # only first 1000 names
+  name_backbone_checklist() %>% # match to backbone
+  filter(!matchType == "NONE") %>% # get matched names
+  pull(usageKey)
 
 # gbif_taxon_keys should be a long vector like this c(2977832,2977901,2977966,2977835,2977863)
 
 # download the data
-occ_download(
-pred_in("taxonKey", gbif_taxon_keys), # important to use pred_in
-pred("hasCoordinate", TRUE),
-pred("hasGeospatialIssue", FALSE),
-format = "SIMPLE_CSV"
+download_info <- occ_download(
+  pred_in("taxonKey", gbif_taxon_keys), # important to use pred_in
+  pred("hasCoordinate", TRUE),
+  pred("hasGeospatialIssue", FALSE),
+  format = "SIMPLE_CSV"
 )
-
-
-
